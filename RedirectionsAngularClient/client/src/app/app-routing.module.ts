4c17ce6ae0682c0f0e44e2fbde5ba544564a08bf
Dufour <oledufour@hotmail.com>
2019-09-28 22:36:14 +0200

    kkk

--- RedirectionsAngularClient/client/src/app/app-routing.module.ts
+++ RedirectionsAngularClient/client/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent }
   , { path: 'crud', loadChildren: () => import(`./crudFeatureModule/crud.module`).then(x => x.CrudModule) ,  canActivate:[AuthGardService]}
   , { path: '**', component:PageNotFoundComponent }
-  // , { path: 'importer', loadChildren: () => import(`./crudFeatureModule/crud.module`).then(x => x.CrudModule) }
+  // ,   { path: 'importer', loadChildren: () => import(`./crudFeatureModule/crud.module`).then(x => x.CrudModule) }
   // , { pfffath: 'publier', loadChildren: () => import(`./crudFeatureModule/crud.module`).then(x => x.CrudModule) }
 ];
 
